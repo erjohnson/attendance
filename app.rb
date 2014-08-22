f6d09bac0742ac99773128404ce5196f96371815
Eric <erjohnson.pdx@gmail.com>
2014-08-22 09:47:00 -0700

    Create view folder and files

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/../cli/*.rb'].each { |file| require file }
 
 database_configurations = YAML::load(File.open('./db/config.yml'))
 development_configuration = database_configurations['development']
