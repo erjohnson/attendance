4268e5f5dac913e76e3515110ce623ed31d92423
Eric <erjohnson.pdx@gmail.com>
2014-08-22 10:42:56 -0700

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822002342) do
+ActiveRecord::Schema.define(version: 20140822174142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140822002342) do
     t.string   "season"
     t.datetime "start"
     t.datetime "end"
+    t.boolean  "current_term"
   end
 
   create_table "users", force: true do |t|
