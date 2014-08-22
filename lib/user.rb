6469456fadace717707a478b68c7e0bb327a90b5
Eric <erjohnson.pdx@gmail.com>
2014-08-22 09:36:33 -0700

    Add validates name to User

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+
   belongs_to :term
   has_many :check_ins
   has_many :days, through: :check_ins
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,4 +4,5 @@ describe User do
   it { should belong_to :term }
   it { should have_many :days }
   it { should have_many :check_ins }
+  it { should validate_presence_of :name }
 end
