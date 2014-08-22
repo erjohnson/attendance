9bb72ed0207618266a97131173464c000d095d21
Eric <erjohnson.pdx@gmail.com>
2014-08-22 12:01:06 -0700

    Add update student ui

--- cli/user_cli.rb
+++ cli/user_cli.rb
@@ -6,10 +6,13 @@ def users_menu
   list_users
   puts "    N > New Student"
   puts "    R > Remove Student"
+  puts "    U > Update Student"
   puts "    M > Main Menu"
   case gets.chomp.upcase
   when 'N'
     create_user
+  when 'U'
+    update_user
   when 'R'
     delete_user
   when 'M'
@@ -42,6 +45,36 @@ def create_user
   puts "#{name} has been added!"
   sleep 0.5
   users_menu
+end
+
+def update_user
+  header
+  puts "    Update A Student    "
+  puts "------------------------"
+  whitespace
+  list_users
+  puts "U > Update Student"
+  puts "M > Users Menu"
+  case gets.chomp.upcase
+  when 'U'
+    puts "Enter the number of the student to update:"
+    user = User.find_by(id: gets.chomp.to_i)
+    whitespace
+    puts "N > Change Name"
+    puts "T > Change Term"
+    case gets.chomp.upcase
+    when 'N'
+      puts "Enter a new name:"
+      user.update(name: gets.chomp)
+      update_user
+    when 'T'
+      puts "Enter a term number:"
+      user.update(term_id: gets.chomp.to_i)
+      update_user
+    end
+  when 'M'
+    users_menu
+  end
 end
 
 def delete_user
