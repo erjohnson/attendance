4d27a8d8e4e8840ab06ca15bcc14716a210bb54c
Eric <erjohnson.pdx@gmail.com>
2014-08-22 10:26:59 -0700

    Add total day count method to term

--- lib/term.rb
+++ lib/term.rb
@@ -2,4 +2,8 @@ class Term < ActiveRecord::Base
   has_many :users
   has_many :days
   has_many :check_ins, through: :days
+
+  def total_days
+    self.days.count
+  end
 end
--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -4,4 +4,12 @@ describe Term do
   it { should have_many :users }
   it { should have_many :days }
   it { should have_many :check_ins }
+
+  describe '.total_days' do
+    it 'returns the total number of days' do
+      term = Term.create(season: 'Spring 2014')
+      day = Day.create(term_id: term.id)
+      expect(term.total_days).to eq 1
+    end
+  end
 end
